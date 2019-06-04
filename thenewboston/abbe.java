f7c95fdedb7cc83dbe2dce97d0f6fe4468e7bc6c
Dominique Persson <niquepersson@gmail.com>
2019-06-04 16:27:01 +0200

    Create abbe.java

---
+++ thenewboston/abbe.java
@@ -0,0 +1,12 @@
+package bucky;
+
+public class abbe {
+	public int sayHiToAb() {
+		System.out.println("Hej Abrahem");
+		return 5;
+	}
+	public void AbSaysHi() {
+		System.out.println("Hallo mein liebling");
+	}
+
+}
