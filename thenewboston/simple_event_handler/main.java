b27b41d740f96e3241d393eaa6fe0fb262cfc312
Dominique Persson <niquepersson@gmail.com>
2019-06-15 15:42:56 +0200

    Create main.java

---
+++ thenewboston/simple_event_handler/main.java
@@ -0,0 +1,15 @@
+package GUI_playground;
+import javax.swing.JFrame;
+
+public class GUI_playground {
+	public static void main(String[] args) {
+		
+		tun object = new tun();
+		object.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		object.setSize(350, 100);
+		object.setVisible(true);
+		
+		
+	}
+
+}
