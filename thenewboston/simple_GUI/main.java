80f4eb2e041d6296cb629a061cf7b6541c465e81
Dominique Persson <niquepersson@gmail.com>
2019-06-15 15:09:06 +0200

    Create main.java

---
+++ thenewboston/simple_GUI/main.java
@@ -0,0 +1,14 @@
+package GUI_playground;
+import javax.swing.JOptionPane;
+import javax.swing.JFrame;
+
+public class GUI_playground {
+	public static void main(String[] args) {
+		
+		tun object = new tun();
+		object.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		object.setSize(275,180);
+		object.setVisible(true);
+	}
+
+}
