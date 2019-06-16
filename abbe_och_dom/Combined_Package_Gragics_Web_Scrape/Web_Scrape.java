35f3f0388fd2b4aa12e1a83f5fb898c5f03a3bd0
Dominique Persson <niquepersson@gmail.com>
2019-06-16 15:40:43 +0200

    Create Web_Scrape.java

---
+++ abbe_och_dom/Combined_Package_Gragics_Web_Scrape/Web_Scrape.java
@@ -0,0 +1,52 @@
+package Combined_Package_Gragics_Web_Scrape;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+public class Web_Scrape {
+	public static String[] img_addr_storage = new String[10000];
+	Teacher[] teacher_arr = new Teacher[1000];
+	
+	public static void scrapy_scrape() {
+		Scanner input = new Scanner(System.in);
+		print("Insert link : ");
+		String url = input.nextLine();
+		//print("Insert css query : ");
+		//String cssQuery = input.nextLine();
+		
+		print("Running...");
+		Document document;
+		int amountofhits = 0;
+		
+		try {
+			document = Jsoup.connect(url).get();
+			String title = document.title();
+			print("Title : " + title);
+			
+			Elements img = document.select("img");
+			String img_addr;
+			
+			for(int e = 0; e < img.size(); e++) {
+				img_addr = img.get(e).absUrl("src");
+				print("img src for teach " + e + " " + img_addr);
+				img_addr_storage[e] = img_addr;
+			}
+			
+		} catch (IOException e){
+			e.printStackTrace();
+		}
+		System.out.println(amountofhits);
+		print("Done");
+		
+		print("\n");
+		
+	}
+	public static void print(String string) {
+		System.out.println(string);
+	}
+
+}
