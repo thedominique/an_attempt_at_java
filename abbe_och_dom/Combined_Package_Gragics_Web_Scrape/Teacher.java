42bbf0fdb5a7ed6742a89818cf84c2d93e0be43f
Dominique Persson <niquepersson@gmail.com>
2019-06-16 15:41:03 +0200

    Create Teacher.java

---
+++ abbe_och_dom/Combined_Package_Gragics_Web_Scrape/Teacher.java
@@ -0,0 +1,17 @@
+package Combined_Package_Gragics_Web_Scrape;
+
+public class Teacher {
+	private String img_addr;
+	private String img_src;
+	
+	public void add_img_addr(String new_addr) {
+		img_addr = new_addr;
+	}
+	public void add_img_src(String new_src) {
+		img_src = new_src;
+	}
+	public String get_img_addr() {
+		return img_addr;
+	}
+
+}
