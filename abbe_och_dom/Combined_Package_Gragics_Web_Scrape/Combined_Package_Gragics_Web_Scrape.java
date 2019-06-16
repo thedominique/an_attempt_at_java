c8f0c90e784828f5b0e7542360083237cf28f53e
Dominique Persson <niquepersson@gmail.com>
2019-06-16 15:42:54 +0200

    Update Combined_Package_Gragics_Web_Scrape.java

--- abbe_och_dom/Combined_Package_Gragics_Web_Scrape/Combined_Package_Gragics_Web_Scrape.java
+++ abbe_och_dom/Combined_Package_Gragics_Web_Scrape/Combined_Package_Gragics_Web_Scrape.java
@@ -7,6 +7,8 @@ public class Combined_Package_Gragics_Web_Scrape {
 	public static void main(String[] args) throws IOException {
 		Web_Scrape teach_array = new Web_Scrape();
 		teach_array.scrapy_scrape();
+		Teacher hej = new Teacher();
+		hej.print_a_stupid_msg();
 		
 		String hej = teach_array.img_addr_storage[3];
 		Graphics show_me_love = new Graphics(hej);
