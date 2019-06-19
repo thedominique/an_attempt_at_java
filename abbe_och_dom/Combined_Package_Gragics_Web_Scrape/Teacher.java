16b6c9db3d445f3004e5db21feb750a94531839c
Dominique Persson <niquepersson@gmail.com>
2019-06-19 13:01:21 +0200

    Update Teacher.java

--- abbe_och_dom/Combined_Package_Gragics_Web_Scrape/Teacher.java
+++ abbe_och_dom/Combined_Package_Gragics_Web_Scrape/Teacher.java
@@ -1,7 +1,7 @@
 package Combined_Package_Gragics_Web_Scrape;
 
 public class Teacher {
-	private String img_addr;
+	private static String img_addr;
 	private String img_src;
 	
 	public void add_img_addr(String new_addr) {
@@ -13,7 +13,5 @@ public class Teacher {
 	public String get_img_addr() {
 		return img_addr;
 	}
-	public void print_a_stupid_msg(){
-	System.out.println("Hej abbe");
-	}
+
 }
